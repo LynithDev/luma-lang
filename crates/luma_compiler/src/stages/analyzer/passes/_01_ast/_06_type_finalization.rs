5202487d2de3c3add14c8c1d112c1d783cae367a
LynithDev <61880709+LynithDev@users.noreply.github.com>
2026-02-08 20:58:02 +0100

    fix: forgot to add contextual type inference for literals in type_finalization

--- crates/luma_compiler/src/stages/analyzer/passes/_01_ast/_06_type_finalization.rs
+++ crates/luma_compiler/src/stages/analyzer/passes/_01_ast/_06_type_finalization.rs
@@ -1,4 +1,6 @@
-use crate::{Type, ast::*, stages::analyzer::{AnalyzerContext, AnalyzerPass, passes::_01_ast::TypeInference}};
+use crate::{
+    Type, TypeKind, ast::*, stages::analyzer::{AnalyzerContext, AnalyzerPass, passes::_01_ast::TypeInference}
+};
 
 pub struct TypeFinalization;
 
@@ -24,31 +26,31 @@ impl AstVisitor<'_> for TypeFinalization {
             StmtKind::Var(var_decl) => {
                 let symbol_id = var_decl.symbol.unwrap_id();
 
-                    let type_entry = {
-                        let ty_cache = ctx.type_cache.borrow();
-                        ty_cache.get(symbol_id).cloned().unwrap()
-                    };
+                let type_entry = {
+                    let ty_cache = ctx.type_cache.borrow();
+                    ty_cache.get(symbol_id).cloned().unwrap()
+                };
 
-                    let resolved_ty = ctx.type_cache.borrow_mut().resolve(&type_entry).unwrap();
+                let resolved_ty = ctx.type_cache.borrow_mut().resolve(&type_entry).unwrap();
 
-                    var_decl.ty = Some(Type::unspanned(resolved_ty.clone()));
+                var_decl.ty = Some(Type::unspanned(resolved_ty.clone()));
 
-                    Self::finalize_expr(ctx, &mut var_decl.initializer);
-            },
+                Self::finalize_expr(ctx, Some(&resolved_ty), &mut var_decl.initializer);
+            }
         }
     }
 }
 
 impl TypeFinalization {
-    fn finalize_expr(ctx: &AnalyzerContext, expr: &mut Expr) {
+    fn finalize_expr(ctx: &AnalyzerContext, contextual_type: Option<&TypeKind>, expr: &mut Expr) {
         let ty = match &mut expr.item {
             ExprKind::Assign(assign_expr) => todo!(),
             ExprKind::Binary(binary_expr) => {
-                Self::finalize_expr(ctx, &mut binary_expr.left);
-                Self::finalize_expr(ctx, &mut binary_expr.right);
+                Self::finalize_expr(ctx, contextual_type, &mut binary_expr.left);
+                Self::finalize_expr(ctx, contextual_type, &mut binary_expr.right);
 
                 binary_expr.left.ty.clone().unwrap()
-            },
+            }
             ExprKind::Block(block_expr) => todo!(),
             ExprKind::Call(call_expr) => todo!(),
             ExprKind::Get(get_expr) => todo!(),
@@ -57,11 +59,11 @@ impl TypeFinalization {
                 let symbol_id = ident_expr.symbol.unwrap_id();
                 let type_entry = ctx.type_cache.borrow().get(symbol_id).cloned().unwrap();
                 ctx.type_cache.borrow_mut().resolve(&type_entry).unwrap()
-            },
+            }
             ExprKind::If(if_expr) => todo!(),
             ExprKind::Literal(literal_expr) => {
-                TypeInference::infer_literal_type(None, literal_expr).unwrap()
-            },
+                TypeInference::infer_literal_type(contextual_type, literal_expr).unwrap()
+            }
             ExprKind::Struct(struct_expr) => todo!(),
             ExprKind::TupleLiteral(tuple_expr) => todo!(),
             ExprKind::Unary(unary_expr) => todo!(),
